a68201b9b43f57fb8c3dba95c690d79ef90c6962
Rodrigo Biffi <rodrigo.biffi@arundo.com>
2020-02-02 18:53:11 +0100

    add server

---
+++ src/server/server.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const WebSocketServer = require('websocket').server;
+
+const server = http.createServer();
+const wsServer = new WebSocketServer({
+  httpServer: server
+});
+
+wsServer.on('request', function(request) {
+  const connection = request.accept(null, request.origin);
+
+  connection.on('message', function(message) {
+    const msg = JSON.parse(message.utf8Data)
+    console.log('Received Message:', msg.msg);
+    connection.sendUTF('Hi this is WebSocket server!');
+  });
+
+  connection.on('close', function(reasonCode, description) {
+    console.log('Client has disconnected.');
+  });
+});
+
+server.listen(8080);
