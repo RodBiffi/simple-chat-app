2dd45c9d82a551e022084dcfe8df23bf215ccc55
Rodrigo Biffi <rodrigo.biffi@arundo.com>
2020-04-20 11:34:28 +0200

    MessageContext scaffolding

--- src/client/App/context/MessagesContext.jsx
+++ src/client/App/context/MessagesContext.jsx
@@ -1,13 +1,50 @@
 import React from 'react';
 import { Context as SocketContext } from './SocketContext';
 
+const messageTypes = {
+    ADD: 'add',
+    EDIT: 'edit',
+    DELETE: 'delete',
+};
+
+const addMessage = (sendHandler) => (message) => {
+
+};
+
+const editMessage = (sendHandler) => (message) => {
+
+};
+
+const deleteMessage = (sendHandler) => (message) => {
+
+};
+
+const contextInitialState = {
+    messages: [],
+    addMessage: () => {},
+    editMessage: () => {},
+    deleteMessage: () => {},
+};
+
+export const Context = React.createContext(contextInitialState);
+
 export default (props) => {
     const { children } = props;
-    const { connected } = React.useContext(SocketContext);
+    const { socket } = React.useContext(SocketContext);
+    const [contextState, setContextState] = React.useState(contextInitialState);
+
+    React.useEffect(() => {
+        setContextState({
+            ...contextState,
+            addMessage: addMessage(socket.send),
+            editMessage: editMessage(socket.send),
+            deleteMessage: deleteMessage(socket.send),
+        });
+    }, []);
+
     return (
-        <div>
-            <div>{ connected ? 'Connected' : 'Disconnected'}</div>
-            <div>{ children }</div>
-        </div>
+        <Context.Provider value={contextState}>
+            { children }
+        </Context.Provider>
     );
 };
